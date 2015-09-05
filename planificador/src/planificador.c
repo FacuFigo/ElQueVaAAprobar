61fdc288ad1882d2053ad736cb09be1d23b133d9
MartinCallegari <martin.callegari10@live.com>
2015-09-05 12:48:33 -0300

    Corrijo socket servidor

--- planificador/src/planificador.c
+++ planificador/src/planificador.c
@@ -19,6 +19,7 @@
 #include <signal.h>
 #include <errno.h>
 #include <sys/types.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <sys/wait.h>
@@ -35,9 +36,11 @@ t_log* archivoLog;
 char* puertoEscucha;
 char* algoritmo;
 int quantum;
+int listeningSocket;
 
 void configurarPlanificador(char* config);
-int configurarSocketServidor(int listeningSocket);
+int configurarSocketServidor();
+//int manejoDeConsola();
 
 int main(int argc, char** argv) {
 
@@ -45,66 +48,82 @@ int main(int argc, char** argv) {
 	archivoLog = log_create("log_Planificador", "Planificador", 1, 0);
 	log_info(archivoLog, "Archivo de logs creado.\n");
 	//Chequeo de argumentos
-	if(argc < 1){
+	if (argc < 1) {
 		log_error(archivoLog, "Falta el archivo de configuraciones.\n");
 	}
 
 //TODO Leer archivo de configuracion y extraer variables
 	configurarPlanificador(argv[1]);
 
+//TODO Preguntar si se termina el programa o hay que reintentar.
+	if (configurarSocketServidor())
+		log_info(archivoLog, "Servidor creado.\n");
+	else
+		log_error(archivoLog, "No se pudo crear el Servidor.\n");
+
+	char* prueba = "\0";
+
+	for(;;){
+		recv(listeningSocket, prueba, sizeof(5), 0);
+	}
+
+//TODO Esperar la conexion de CPUs
+
+//TODO Levantar la consola
+	//pthread_t hiloConsola;
+
+	//pthreadCreate(&hiloConsola, NULL, (void *) manejoDeConsola, NULL);
+
 	return 0;
 
 }
 
-void configurarPlanificador(char* config){
+void configurarPlanificador(char* config) {
 
 	t_config* configPlanificador = config_create(config);
-	if(config_has_property(configPlanificador,"PUERTO_ESCUCHA"))
-		puertoEscucha = config_get_string_value(configPlanificador, "PUERTO_ESCUCHA");
-	if(config_has_property(configPlanificador, "ALGORITMO_PLANIFICADOR"))
-		algoritmo = config_get_string_value(configPlanificador, "ALGORITMO_PLANIFICADOR");
-	if(config_has_property(configPlanificador, "QUANTUM"))
+	if (config_has_property(configPlanificador, "PUERTO_ESCUCHA"))
+		puertoEscucha = config_get_string_value(configPlanificador,
+				"PUERTO_ESCUCHA");
+	if (config_has_property(configPlanificador, "ALGORITMO_PLANIFICADOR"))
+		algoritmo = config_get_string_value(configPlanificador,
+				"ALGORITMO_PLANIFICADOR");
+	if (config_has_property(configPlanificador, "QUANTUM"))
 		quantum = config_get_int_value(configPlanificador, "QUANTUM");
 	config_destroy(configPlanificador);
 }
 
-int configurarSocketServidor(int listeningSocket){
-	int status;
-	int yes = 1;
-	struct addrinfo hints, *serverInfo, *p;
+int configurarSocketServidor() {
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
+	struct sockaddr_in direccionServidor;
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = INADDR_ANY;
+	direccionServidor.sin_port = htons((int) puertoEscucha);
 
-	if((status = getaddrinfo(NULL, puertoEscucha, &hints, &serverInfo)) == -1) {
-	   return 0;
-	}
-	for(p = serverInfo; p != NULL; p = p->ai_next) {
-	if((listeningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol)) == -1){
-		return 0;
-	}
-	//fcntl(listeningSocket, F_SETFD, O_NONBLOCK);
-	//No estoy seguro de esta funcion
-	if (setsockopt(listeningSocket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-	    return 0;
-	}
-	if((bind(listeningSocket, serverInfo->ai_addr, serverInfo->ai_addrlen)) == -1){
-		return 0;
-	}
-	break;
+	int servidor = socket(AF_INET, SOCK_STREAM, 0);
+
+	int activado = 1;
+	setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+
+	if (bind(servidor, (void*) &direccionServidor, sizeof(direccionServidor)) != 0){
+		log_error(archivoLog, "Falló el bind");
+		return 1;
 	}
 
-    if (p == NULL)  {
-       	fprintf(stderr, "server: failed to bind\n");
-    }
+	listen(servidor, (int) puertoEscucha);
 
-	freeaddrinfo(serverInfo);
+	struct sockaddr_in direccionCliente;
+	unsigned int len;
+	listeningSocket = accept(servidor, (void*) &direccionCliente, &len);
 
-	if ((listen(listeningSocket, BACKLOG)) == -1){
-			log_error(archivoLog, "Error en el listen");
-	}
 	return 1;
 }
 
+/*int manejoDeConsola(){
+ char* comando;
+
+ gets(comando);
+ string_starts_with(comando,"correr");
+
+ return 0;
+ }
+ */
