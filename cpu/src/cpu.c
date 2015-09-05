7e3446531a13222c12ac58fda1e3a3ce796be0ce
RamiroCabello <ramirovip4@hotmail.com>
2015-09-05 11:37:54 -0300

    Agrego configuracion de cpu

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -28,13 +28,66 @@
 #include <commons/string.h>
 #include <commons/collections/list.h>
 
+t_log* archivoLog;
+char* ipPlanificador;
+char* puertoPlanificador;
+char* ipMemoria;
+char* puertoMemoria;
+int cantidadHilos;
+int retardo;
+
+void configurarCPU(char* config);
 int configurarSocketCliente(int s, char* ip, char* puerto);
 
 int main(int argc, char** argv) {
 
+	//Creo el archivo de logs
+		archivoLog = log_create("log_CPU", "CPU", 1, 0);
+		log_info(archivoLog, "Archivo de logs creado.\n");
+		//Chequeo de argumentos
+		if(argc < 1){
+			log_error(archivoLog, "Falta el archivo de configuraciones.\n");
+		}
+
+	//TODO Leer archivo de configuracion y extraer variables
+		configurarCPU(argv[1]);
+
+
+
+
+
 	return 0;
 
 }
+
+void configurarCPU (char* config){
+
+	t_config* configCPU = config_create(config);
+	if(config_has_property(configCPU,"IP_PLANIFICADOR"))
+		ipPlanificador = config_get_string_value(configCPU, "IP_PLANIFICADOR");
+	if(config_has_property(configCPU, "PUERTO_PLANIFICADOR"))
+		puertoPlanificador = config_get_string_value(configCPU, "PUERTO_PLANIFICADOR");
+	if(config_has_property(configCPU, "IP_MEMORIA"))
+		ipMemoria = config_get_string_value(configCPU, "IP_MEMORIA");
+	if(config_has_property(configCPU, "PUERTO_MEMORIA"))
+			puertoPlanificador = config_get_string_value(configCPU, "PUERTO_MEMORIA");
+	if(config_has_property(configCPU, "CANTIDAD_HILOS"))
+			cantidadHilos = config_get_int_value(configCPU, "CANTIDAD_HILOS");
+	if(config_has_property(configCPU, "RETARDO"))
+				retardo = config_get_int_value(configCPU, "RETARDO");
+
+
+	config_destroy(configCPU);
+}
+
+
+
+
+
+
+
+
+
 
 int configurarSocketCliente(int s, char* ip, char* puerto){
 	int status;
