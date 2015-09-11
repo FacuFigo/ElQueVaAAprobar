11444ba590562f96eb82cc133e6a407f8773b2ae
GasKitela <gas.kitela@gmail.com>
2015-09-11 12:51:54 -0300

    cambios en admins

--- admSwap/src/admSwap.c
+++ admSwap/src/admSwap.c
@@ -34,50 +34,52 @@
 t_log* archivoLog;
 char* ipAdmMemoria;
 char* puertoAdmMemoria;
-char* puertoEscucha;
+int puertoEscucha;
 char* nombreSwap;
 int cantidadPaginas;
+int listeningSocket;
 int tamanioPagina;
 unsigned retardoCompactacion; //son segundos sino lo cambio a int
 
 void configurarAdmSwap(char* config);
-int configurarSocketServidor(int listeningSocket);
+int configurarSocketServidor();
 
 int main(int argc, char** argv) {
 	//Creo el archivo de logs
-			archivoLog = log_create("log_SWAP", "SWAP", 1, 0);
-			log_info(archivoLog, "Archivo de logs creado.\n");
-			//Chequeo de argumentos
-			if(argc < 1){
-				log_error(archivoLog, "Falta el archivo de configuraciones.\n");
-			}
 
-		//TODO Leer archivo de configuracion y extraer variables
-			configurarAdmSwap(argv[1]);
-		if (configurarSocketServidor(ipAdmMemoria, puertoAdmMemoria,
-						&socketAdmMemoria))
-					log_info(archivoLog, "Se me conectó el Admin de memoria %i.\n",
-							socketPlanificador);
-		else
-					log_error(archivoLog, "Error al conectarseme en el Admin de memoria. %s\n",
-							ipAdmMemoria);
+	archivoLog = log_create("log_SWAP", "SWAP", 1, 0);
+	log_info(archivoLog, "Archivo de logs creado.\n");
+	//Chequeo de argumentos
 
+	//TODO Leer archivo de configuracion y extraer variables
+	configurarAdmSwap(argv[1]);
 
-		return 0;
+	if (argc < 1) {
+		log_error(archivoLog, "Falta el archivo de configuraciones.\n");
+	}
+
+	configurarSocketServidor();
+
+	return 0;
 
 }
 
 void configurarAdmSwap(char* config) {
 	t_config* configurarAdmSwap = config_create(config);
 	if (config_has_property(configurarAdmSwap, "NOMBRE_SWAP"))
-		nombreSwap = string_duplicate(config_get_string_value(configurarAdmSwap, "NOMBRE_SWAP"));
+		nombreSwap = string_duplicate(
+				config_get_string_value(configurarAdmSwap, "NOMBRE_SWAP"));
 	if (config_has_property(configurarAdmSwap, "PUERTO_ESCUCHA"))
-		puertoEscucha = config_get_string_value(configurarAdmSwap,
+		puertoEscucha = config_get_int_value(configurarAdmSwap,
 				"PUERTO_ESCUCHA");
 	if (config_has_property(configurarAdmSwap, "IP_MEMORIA"))
-		ipAdmMemoria = string_duplicate(config_get_string_value(configurarAdmSwap, "IP_MEMORIA"));
+		ipAdmMemoria = string_duplicate(
+				config_get_string_value(configurarAdmSwap, "IP_MEMORIA"));
 	if (config_has_property(configurarAdmSwap, "PUERTO_MEMORIA"))
-		puertoAdmMemoria = string_duplicate(config_get_string_value(configurarAdmSwap, "PUERTO_MEMORIA"));
+		puertoAdmMemoria = string_duplicate(
+				config_get_string_value(configurarAdmSwap, "PUERTO_MEMORIA"));
+	puertoAdmMemoria = config_get_string_value(configurarAdmSwap,
+			"PUERTO_MEMORIA");
 	if (config_has_property(configurarAdmSwap, "CANTIDAD_PAGINAS"))
 		cantidadPaginas = config_get_int_value(configurarAdmSwap,
 				"CANTIDAD_PAGINAS");
@@ -91,42 +93,29 @@ void configurarAdmSwap(char* config) {
 	config_destroy(configurarAdmSwap);
 }
 
-int configurarSocketServidor(int listeningSocket){
-	int status;
-	int yes = 1;
-	struct addrinfo hints, *serverInfo, *p;
+int configurarSocketServidor() {
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
+	struct sockaddr_in direccionServidor;
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = INADDR_ANY;
+	direccionServidor.sin_port = htons(puertoEscucha);
 
-	if((status = getaddrinfo(NULL, puertoEscucha, &hints, &serverInfo)) == -1) {
-	   return 0;
-	}
-	for(p = serverInfo; p != NULL; p = p->ai_next) {
-	if((listeningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol)) == -1){
-		return 0;
-	}
-	//fcntl(listeningSocket, F_SETFD, O_NONBLOCK);
-	//No estoy seguro de esta funcion
-	if (setsockopt(listeningSocket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-	    return 0;
-	}
-	if((bind(listeningSocket, serverInfo->ai_addr, serverInfo->ai_addrlen)) == -1){
-		return 0;
-	}
-	break;
+	listeningSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	int activado = 1;
+	setsockopt(listeningSocket, SOL_SOCKET, SO_REUSEADDR, &activado,
+			sizeof(activado));
+
+	if (bind(listeningSocket, (void*) &direccionServidor,
+			sizeof(direccionServidor)) != 0) {
+		log_error(archivoLog, "Falló el bind");
+		return 1;
 	}
 
-    if (p == NULL)  {
-       	fprintf(stderr, "server: failed to bind\n");
-    }
+	listen(listeningSocket, BACKLOG);
 
-	freeaddrinfo(serverInfo);
+	log_info(archivoLog, "Servidor creado. %i\n", listeningSocket);
 
-	if ((listen(listeningSocket, BACKLOG)) == -1){
-			log_error(archivoLog, "Error en el listen");
-	}
 	return 1;
 }
+
